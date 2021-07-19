c788cecbf274707411cf245c9dbb490357b468f0
ClaudioZeni <claudiopuntozeni@gmail.com>
2021-07-19 16:55:03 +0200

    First attempt at juliaACE + python interface

---
+++ notebooks/utils.jl
@@ -0,0 +1,111 @@
+
+# using Pkg
+# Pkg.activate("/home/claudio/postdoc/ACE.jl/Project.toml")
+
+using ACE: alloc_B, alloc_temp, evaluate!,alloc_dB, alloc_temp_d, evaluate_d!
+using NeighbourLists: maxneigs
+using JuLIP:  neighbourlist, cutoff, JVec, AbstractAtoms, fltype, AtomicNumber
+using JuLIP.Potentials: neigsz!
+using IPFitting.Data: read_xyz
+
+function sum_descriptor(shipB, at::AbstractAtoms{T}) where {T}
+   E = zeros(fltype(shipB), length(shipB))
+   B = alloc_B(shipB)
+   nlist = neighbourlist(at, cutoff(shipB))
+   maxnR = maxneigs(nlist)
+   tmp = alloc_temp(shipB, maxnR)
+   tmpRZ = (R = zeros(JVec{T}, maxnR), Z = zeros(AtomicNumber, maxnR))
+   for i = 1:length(at)
+      j, R, Z = neigsz!(tmpRZ, nlist, at, i)
+      fill!(B, 0)
+      evaluate!(B, tmp, shipB, R, Z, at.Z[i])
+      E[:] .+= B[:]
+   end
+   return E
+end
+
+function sum_descriptor_traj(basis, traj)
+    A_all = Array{Float64, 2}(undef, length(traj), length(basis)) 
+        atoms = traj[i].at
+        A_all[i, :] = sum_descriptor(basis, atoms)
+    return A_all
+end
+
+function sum_d_descriptor(shipB, at::AbstractAtoms{T}) where {T}
+   # precompute the neighbourlist to count the number of neighbours
+   nlist = neighbourlist(at, cutoff(shipB); storelist=false)
+   maxR = maxneigs(nlist)
+   # allocate space accordingly
+   F = zeros(JVec{T}, length(at), length(shipB))
+   B = alloc_B(shipB, maxR)
+   dB = alloc_dB(shipB, maxR)
+   tmp = alloc_temp_d(shipB, maxR)
+   tmpRZ = (R = zeros(JVec{T}, maxR), Z = zeros(AtomicNumber, maxR))
+   return sum_d_descriptor_inner!(shipB, at, nlist, F, B, dB, tmp, tmpRZ)
+end
+
+# this is a little hack to remove a type instability. It probably makes no
+# difference in practise...
+function sum_d_descriptor_inner!(shipB, at::AbstractAtoms{T},
+                       nlist, F, B, dB, tmp, tmpRZ) where {T}
+   # assemble site gradients and write into F
+   for i = 1:length(at)
+      j, R, Z = neigsz!(tmpRZ, nlist, at, i)
+      fill!(dB, zero(JVec{T}))
+      fill!(B, 0)
+      evaluate_d!(B, dB, tmp, shipB, R, Z, at.Z[i])
+      for a = 1:length(R)
+         F[j[a], :] .-= dB[:, a]
+         F[i, :] .+= dB[:, a]
+      end
+   end
+   return [ F[:, iB] for iB = 1:length(shipB) ]
+end
+
+function sum_d_descriptor_traj(basis, traj)
+    dA_all = []
+        atoms = traj[i].at
+        XF = sum_d_descriptor(basis, atoms);
+        XF = hcat([collect(Iterators.flatten(a)) for a in XF]...)
+        push!(dA_all, XF)
+    return dA_all
+end
+
+function fit_potential(XE_tr, YE_tr, XF_tr, YF_tr, alpha=1.0)
+    YF_tr = hcat(YF_tr'...)'
+    XF_tr = hcat(XF_tr'...)'
+    X_tr = vcat(XE_tr, XF_tr)
+    Y_tr = vcat(YE_tr, YF_tr);
+    ridge_pred = fit!(Ridge(alpha), X_tr, Y_tr)
+    return ridge_pred
+end
+
+function predict_potential(ridge_pred, XE_tst, XF_tst)
+    n_struc = length(XE_tst[:, 1])
+    XF_tst = hcat(XF_tst'...)'
+    X_tst = vcat(XE_tst, XF_tst)
+    result = predict(ridge_pred, X_tst)
+    return result[1:n_struc], result[n_struc+1:end]
+end
+
+function extract_info(B, traj)
+    XE = sum_descriptor_traj(B, traj);
+    XF = sum_d_descriptor_traj(B, traj);
+
+    YE = Vector{Float64}(undef, length(traj)) 
+    for i in 1:length(traj)
+        YE[i] = traj[i].D["E"][1]
+    end
+
+    YF = Vector{Float64}[]
+    for i in 1:length(traj)
+        push!(YF, traj[i].D["F"])
+    end
+
+    nat = Vector{Float64}(undef, length(traj)) 
+    for i in 1:length(traj)
+        nat[i] = length(traj[i])
+    end
+    
+    return XE, YE, XF, YF, nat
+    end;
